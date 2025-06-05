54c9f969eaef9348574163e30c2a9236ebec9629
emperorbj <cwaku96@gmail.com>
2025-06-05 19:04:48 +0100

    added the llm response generation from vector db

---
+++ langchain-course/rag_one.py
@@ -0,0 +1,59 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import HumanMessage,AIMessage,SystemMessage
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.vectorstores import FAISS
+from dotenv import load_dotenv
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableMap,RunnableLambda,RunnableParallel
+import os
+from langchain_cohere import CohereEmbeddings
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import CharacterTextSplitter
+
+
+
+load_dotenv()
+embeddings = CohereEmbeddings(
+    model="embed-v4.0",
+    cohere_api_key= os.getenv("COHERE_API_KEY")
+)
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(current_dir,"docs.txt")
+persistent_path = os.path.join(current_dir,"faiss_index")
+
+if not os.path.exists(persistent_path):
+    print("Path cannot be found")
+    
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(
+            f"this {file_path} cannot be found"
+        )
+    
+    loader = TextLoader(file_path)
+    document = loader.load()
+    
+    
+    splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    docs = splitter.split_documents(document)
+
+    print("\n----Document Embedding and Indexing----")
+    print(f"\nNumber of chunks of document: {len(docs)}")
+    print(f"Sample chunk:\n---\n{docs[0].page_content}\n---")
+    
+    splitter = CharacterTextSplitter(chunk_size = 1000, chunk_overlap = 0)
+    docs = splitter.split_documents(document)
+    
+    print("\n ----Document embedding----")
+    print(f"\n Number of chunks of document:{len(docs)} ")
+    print(f"sample chunk\n {docs[0].page_content}")
+    
+    dummy_text_embedding = embeddings.embed_query("test")
+    embedding_dimension = len(dummy_text_embedding)
+    
+    
+    vector_store = FAISS.from_documents(docs, embeddings)
+    vector_store.save_local(persistent_path)
+else:
+    print("FAISS vector db exists. No need to initialize")
+    vector_store = FAISS.load_local(persistent_path, embeddings, allow_dangerous_deserialization=True)
